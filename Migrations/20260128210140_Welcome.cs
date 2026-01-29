b128965661f6de600af14f1b512478b73fe660db
elessawy <melessawyh@gmail.com>
2026-01-29 07:52:54 +0300

    last 29-1-2026

---
+++ Migrations/20260128210140_Welcome.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Welcome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "DoctorInfos",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "DoctorInfos");
+        }
+    }
+}
